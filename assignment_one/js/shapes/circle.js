f377c324dad33ef7f902b86dd526c2138d1826fa
tylerlaberge <tyler.laberge@maine.edu>
2016-09-13 17:20:02 -0400

    improved circle shading effect.

--- assignment_one/js/shapes/circle.js
+++ assignment_one/js/shapes/circle.js
@@ -3,7 +3,7 @@ function Circle(gl, shader_program,
     this.vertex_size = 2;
     this.color_size = 4;
     this.vertices = this.compute_vertices(center_vertex, radius);
-    this.colors = this.compute_colors(color, this.vertices.length);
+    this.colors = this.compute_colors(color, this.vertices.length / this.vertex_size);
     Shape.call(
         this,
         gl, shader_program,
@@ -20,11 +20,15 @@ Circle.prototype.compute_colors = function (base_color, num_vertices) {
     var color_scale = .99;
     for (var i = 0; i < num_vertices; i++){
         colors.push(next_color, base_color[1], base_color[2], base_color[3]);
-        if (i < ((num_vertices-1)/2)){
-            next_color = next_color*color_scale;
-        }
-        else{
-            next_color = next_color/color_scale;
+        if(i%2){
+            if (i < num_vertices/2){
+                next_color = next_color*color_scale;
+                color_scale -= .005;
+            }
+            else{
+                next_color = next_color/color_scale;
+                color_scale += .005;
+            }
         }
     }
     return colors;
